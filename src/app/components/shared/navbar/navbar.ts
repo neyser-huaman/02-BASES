798fd80192a0973a67f283c01c5945701a4d2c32
neyser-huaman <neyser@outlook.com>
2025-07-13 05:57:25 +0200

    Page Dragonball

--- src/app/components/shared/navbar/navbar.ts
+++ src/app/components/shared/navbar/navbar.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
-  templateUrl: './navbar.html'
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
 })
 export class Navbar {
 
